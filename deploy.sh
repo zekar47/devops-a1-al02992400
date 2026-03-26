02f54c823198beb68a421cebb73deb82fefc9775
zekar <zekar@horsefucker.org>
2026-03-25 20:22:04 -0600

    Fixed a variable in deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
+set -e
 # Variables
-BUCKET_NAME="${BUCKET_NAME:-al02992400-a1}"
+BUCKET_NAME="${BUCKET_NAME:-"al02992400-a1"}"
+REGION="${REGION:-"us-east-1"}"
 
 # Crear bucket si no existe
 aws s3 mb "s3://$BUCKET_NAME" --region "$REGION" 2>/dev/null || true
@@ -14,4 +16,4 @@ aws s3 cp index.html "s3://$BUCKET_NAME"
 aws s3 cp style.css "s3://$BUCKET_NAME"
 
 echo "Sitio desplegado en:"
-echo "http://$BUCKET_NAME.s3-website-us-east-1.amazonaws.com"
+echo "http://$BUCKET_NAME.s3-website-$REGION.amazonaws.com"
